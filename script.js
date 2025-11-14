441660bb8e2e787a9a88ec4698dca8df93a06f36
kk7jji <kk7jji@outlook.com>
2025-11-13 20:24:16 -0800

    Complete TOP Etch-A-Sketch.  The sketch area of the webpage is 500px x 500px and includes a functional reset button which allows the user to select sketch areas from 1 x 1 to 10 x 10 pixels.
    As the mouse passes over each sketch box a
    random color is assigned to the box and it gets
    progressively darker with each change.  The
    sketch box will turn black after a total of 10
    changes.

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
-console.log("Hello World");
+
+const etchASketch = function() {
+
+    bodyHTML = document.querySelector("body");
+    resetButton = document.querySelector(".etch-a-sketch-reset");
+    etchASketchContainer = document.querySelector(".etch-a-sketch-container");
+
+    const removeOldEtchASketch = function () {
+        while (etchASketchContainer.firstChild) {
+            etchASketchContainer.removeChild(etchASketchContainer.firstChild);
+        }
+    }
+
+    const insertNewEtchASketch = function (sketchSize) {
+        etchASketchContainer.style.width = 500;
+        etchASketchContainer.style.height = 500;
+
+        for (let i=0; i < (+sketchSize) ** 2; i++) {
+            etchASketchContainer.appendChild(createUnitBoxDiv(sketchSize));
+        }
+    }
+
+    const createUnitBoxDiv = function (sketchSize) {
+
+        let myWidth = etchASketchContainer.scrollWidth / +sketchSize;
+        let myHeight = etchASketchContainer.scrollHeight / +sketchSize;
+
+        newDiv = document.createElement('div');
+        newDiv.classList.add("etch-a-sketch-unit-box");
+        newDiv.appendChild(document.createTextNode("10"));
+
+        newDiv.style.width = myWidth + "px";
+        newDiv.style.height = myHeight + "px";
+
+        return newDiv;
+    }
+
+    const makeANewEtchASketchArray = function (e) {
+        let userInput = "";
+        while ( isNaN(+userInput) || +userInput < 1 || +userInput > 10 ) {
+            userInput = +prompt("How many squares on a side? (no more than 10)");
+        }
+
+        removeOldEtchASketch();
+        insertNewEtchASketch(+userInput);
+    }
+   
+    const updateUnitBoxColor = function (e) {
+
+        if (!(e.target === etchASketchContainer)) {
+            if (+e.target.innerText > 0) {
+                transparency = +(--e.target.innerText) / 10;
+            } else {
+                transparency = 0;
+            }
+
+            let r = randomInt(0,255);
+            let g = randomInt(0,255);
+            let b = randomInt(0,255);
+
+            e.target.style.backgroundColor = 
+                `rgba(${r},${g},${b},${transparency})`;
+        }
+    }
+
+    const randomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;        
+    }
+
+
+    resetButton.addEventListener('click',makeANewEtchASketchArray);
+    etchASketchContainer.addEventListener('mouseover',updateUnitBoxColor);
+
+}
+
+etchASketch();
